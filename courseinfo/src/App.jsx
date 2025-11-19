2c4853ba5402a04091124f27770bd05a37d8cbf0
NateVoyages <allanromo20@gmail.com>
2025-11-18 20:06:17 -0800

    Add Content and Total components

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,3 +1,29 @@
+  const Header = (props) => {
+
+    return(<h1>
+      {props.course}
+      </h1>)
+  }
+  const Content = (props) => {
+    return(<>
+           <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+      </>)
+    
+  }
+  const Total = (props) => {
+    return(
+          <p>Number of exercises {props.exercises1 + props.exercises2 
+            + props.exercises3}</p>
+    )
+    }
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -30,14 +56,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
+      <Content exercises1 ={exercises1} 
+      exercises2= {exercises2} exercises3= {exercises3}
+      part1= {part1} part2={part2} part3={part3}/>
+      <Total exercises1 ={exercises1} 
+      exercises2= {exercises2} exercises3= {exercises3}/>
     </div>
   )
 }
 
-  const Header = (props) => {
 
-    return(<h1>
-      {props.course}
-      </h1>)
-  }
 export default App
